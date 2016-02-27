d252bdb59b33d801bd926a896820973cecfe56f1
yutokyokutyo <k.yuto.cre@gmail.com>
2016-02-27 13:56:48 +0900

    コンストラクタを追加

--- src/Tddbc/GridPoint.php
+++ src/Tddbc/GridPoint.php
@@ -1,8 +1,17 @@
 <?php
 namespace Tddbc;
 
+
 class GridPoint
 {
+    private $x;
+    private $y;
+
+    public function __construct($x, $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
     public function getX()
     {
         return 4;
